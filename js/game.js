31c2c8e51e51c2628520349e5296aa99655f362e
Nickdev2023 <n.michelpro1@gmail.com>
2023-07-18 13:03:07 +0200

    changes two

--- js/game.js
+++
@@ -1,8 +0,0 @@
-class Game {
-  constructor() {
-    this.player = null;
-    this.obstacles = [];
-    this.score = 0;
-    this.lives = 3;
-  }
-}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const heroMoving = document.querySelector(".hero");
 let positionX = 0;
-const mainContainer = document.querySelector(".background");
+const mainContainer = document.querySelector("#background");
 
 const pressedKeys = { left: false, right: false };
 
@@ -36,26 +36,118 @@ setInterval(() => {
 //
 
 function move(direction) {
-  const containerBounding = background.getBoundingClientRect();
+  const containerBounding = mainContainer.getBoundingClientRect();
   const heroBounding = heroMoving.getBoundingClientRect();
+  // console.log(containerBounding);
 
   switch (direction) {
     case "left":
       if (heroBounding.left <= containerBounding.left) {
-        x = 0;
+        positionX = -30;
       } else {
-        positionX -= 5;
+        positionX -= 10;
       }
       break;
 
     case "right":
       if (heroBounding.right >= containerBounding.right) {
-        x = containerBounding.width - heroBounding.width;
+        positionX = containerBounding.width - heroBounding.width;
       } else {
-        positionX += 5;
+        positionX += 10;
       }
       break;
   }
 
   heroMoving.style.left = `${positionX}px`;
+}
+
+let bottom = 0;
+let gravity = 0.9;
+let isJumping = false;
+
+function jump() {
+  if (isJumping) {
+    return;
+  }
+  let timerIdUp = setInterval(function () {
+    if (bottom > 250) {
+      clearInterval(timerIdUp);
+      let timerIdDown = setInterval(function () {
+        if (bottom < 85) {
+          clearInterval(timerIdDown);
+          bottom = 11;
+          isJumping = false;
+          heroMoving.style.bottom = bottom + "vh";
+          return;
+        }
+        bottom -= 5;
+        heroMoving.style.bottom = bottom + "px";
+      }, 10);
+    }
+    isJumping = true;
+    bottom += 85;
+    bottom *= gravity;
+    heroMoving.style.bottom = bottom + "px";
+  }, 10);
+}
+
+function control(e) {
+  //console.log(e);
+  if (e.key === " ") {
+    jump();
+  }
+}
+window.addEventListener("keydown", control);
+
+const zombieMove = document.querySelector(".zombie");
+let bouncingCircleX = 0;
+let direction = 1;
+let speed = 1;
+const actionContainer = document.querySelector(".actionPart");
+const containerBounding = actionContainer.getBoundingClientRect();
+
+setInterval(() => {
+  const zombieBounding = zombieMove.getBoundingClientRect();
+
+  if (zombieBounding.right >= containerBounding.right) {
+    // console.log(circleBounding.right, containerBounding.right);
+    direction = -1;
+  }
+  if (zombieBounding.left <= containerBounding.left) {
+    direction = 1;
+  }
+  bouncingCircleX += speed * direction;
+
+  zombieMove.style.left = `${bouncingCircleX}px`;
+
+  if (checkCollision()) {
+    console.log("lose life");
+  }
+}, 1000 / 60);
+
+let speedIIntervalId = setInterval(() => {
+  speed *= 1.05;
+  if (speed > 30) {
+    speed = 30;
+  }
+  // if (bouncingCircleX > 1.5 * direction) {
+  //   clearInterval(speed);
+  //   // bouncingCircleX = 1.5 * direction;
+  // }
+}, 2000);
+
+function checkCollision() {
+  const zombieBounding = zombieMove.getBoundingClientRect();
+  const heroBounding = heroMoving.getBoundingClientRect();
+  let isInX =
+    heroBounding.left < zombieBounding.right &&
+    heroBounding.right > zombieBounding.left;
+
+  let isInY =
+    heroBounding.bottom > zombieBounding.top &&
+    heroBounding.top < zombieBounding.bottom;
+
+  if (isInX && isInY) {
+    console.log("touche");
+  }
 }
